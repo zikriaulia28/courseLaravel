69dc12d653c8c6fc1ca2c01565aefcb67e2299f1
zikriaulia28 <zikriaulia98@gmail.com>
2022-08-11 18:26:14 +0700

    login and register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,35 +1,19 @@
 @extends('Layout/isGuest')
 
 @section('content')
-    {{-- <div>
-        <h3>Login Page</h3>
-        <i>{{ session()->get('error') }}</i>
-        <form method="POST" action={{route('login_action')}}>
-            @csrf
-            <input type="text" name="username" placeholder="username">
-            <input type="password" name="password" placeholder="password">
-            <button type="submit">LOGIN</button>
-        </form>
-    </div> --}}
-    <div class="global-container">
-        <div class="card login-form">
-            <div class="card-body">
-                <h1 class="card-title text-center">Register</h1>
-                <div class="card-text">
-                    <form method="POST" action={{route('register_action')}}>
-                        @csrf
-                      <div class="form group mb-3">
-                        <label  class="form-label">Username</label>
-                        <input class="form-control" type="text" name="username" placeholder="username">
-                      </div>
-                      <div class="form group mb-3">
-                        <label class="form-label">Password</label>
-                        <input class="form-control" type="password" name="password" placeholder="password">
-                      </div>
-                      <button type="submit" class="btn btn-primary btn-block">Register</button>
-                    </form> 
-                </div>
-            </div>
+
+    <div>
+        <h1 class="font-bold text-center p-5 text-2xl">Register Here :)</h1>
+        <div class="flex justify-center">
+            <form class="flex flex-col justify-center bg-white rounded shadow-lg shadow-cyan-500/50 p-10" method="POST" action={{route('register_action')}}>
+                <i>{{ session()->get('success') }}</i>
+                @csrf
+                <label class="font-semibold text-xs" for="usernameField">Username</label>
+                <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded" name="username" type="text">
+                <label class="font-semibold text-xs mt-3" for="passwordField">Password</label>
+                <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded" name="password" type="password">
+                <button type="submit" class="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">Register</button>
+            </form>
         </div>
     </div>
 
