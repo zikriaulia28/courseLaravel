027f36f7ccbb5fbaa8564ef3bc3b4cc08c86a5ad
Iqbal Sonata <iqbalsonata2@gmail.com>
2022-08-11 13:39:07 +0700

    Styling Feature/Home

--- app/Http/Controllers/OuterController.php
+++ app/Http/Controllers/OuterController.php
@@ -7,16 +7,23 @@ use Illuminate\Http\Request;
 
 class OuterController extends Controller
 {
-    public function index () 
+    public function index()
     {
-        $articles = Articles::get();
+        $articles = Articles::orderBy('id', 'desc')->paginate(6);
+        $lastItem = $articles->lastPage();
         return view('home', [
             'title' => 'Ini adalah Seluruh Articles',
-            'articles' => $articles
+            'articles' => $articles,
+            'meta'     => [
+                'currentPage'   => $articles->currentPage(),
+                'nextPage'      => $articles->nextPageUrl(),
+                'prevPage'      => $articles->previousPageUrl(),
+            ],
+            'test'          => $articles->getUrlRange(1, $lastItem),
         ]);
     }
 
-    public function article_detail ($id)
+    public function article_detail($id)
     {
         return view('article', [
             'title' => 'ARTIKEL ' . $id,
--- database/migrations/2022_08_01_163325_create_articles_table.php
+++ database/migrations/2022_08_01_163325_create_articles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('tag');
             $table->timestamps();
         });
